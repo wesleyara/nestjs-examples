978c772cf1ab73a546ab9487a93d1b153e914652
wesleyara <wes1gom@gmail.com>
2026-02-24 21:15:24 -0300

    feat: update Prisma configuration and add account migration

--- nestjs-with-jwt-auth-prisma-and-redis/src/infra/prisma/prisma.service.ts
+++ nestjs-with-jwt-auth-prisma-and-redis/src/infra/prisma/prisma.service.ts
@@ -1,9 +1,13 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({ adapter });
   }
 }
